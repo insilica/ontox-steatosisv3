ed5682c7adae8797ab53af83f2df007f4e184f0c
Thomas Luechtefeld <tom@insilica.co>
2024-05-20 08:12:59 -0400

    removed tokens, these are dead now anyways

--- stages/01_load.py
+++ stages/01_load.py
@@ -5,6 +5,3 @@ dotenv.load_dotenv()
 project_id = 123619
 client = sr.Client(os.getenv('SR_ADMIN_TOKEN'))
 client.sync(project_id)
-
-SR_ADMIN_TOKEN="bc46c94cf6c55ccc48cf46c2"
-OPENAI_API_KEY='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'
